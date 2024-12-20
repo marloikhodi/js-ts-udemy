7c27bcaef07c58fda22b4fe1469e21177bacc4ce
marloikhodi <deivid.serafim03@gmail.com>
2024-12-20 10:28:45 -0300

    variaveis

---
+++ section-2/variaveis/variaveis.js
@@ -0,0 +1,25 @@
+let nome //Declaração de variavel
+//var nome = 'Joao' 
+nome = 'Otavio'
+console.log(nome, 'nasceu em 1984.')
+console.log('Em 200', nome, 'conheceu Maria')
+console.log(nome, 'casou-se com Maria em 2012')
+nome = 'Pedro'
+console.log('Maria teve 1 filho com', nome, 'em 2015')
+console.log('O filho de', nome, 'se chama Eduardo')
+
+// Regras de variaveis
+// Não podemos criar variaveis com palavras reservadas (if, else, console, etc)
+// Variaveis precisam ter nomes significativos, auxiliando na criação do codigo
+// Não pode criar variaveis com nome começando em numero
+// Nao podem ter nomes ou espaços
+// Case-sensitive
+// Nao podemos redeclarar variavies com let
+// Nao utilize var, utilize let
+
+let nomeCliente = 12
+let nomecliente = 21
+
+console.log(nomeCliente)
+console.log(nomecliente)
+
