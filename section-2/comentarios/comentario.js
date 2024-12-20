b513ed0cadcfdd3d6d3f3c839bfc01822ff8f44a
marloikhodi <deivid.serafim03@gmail.com>
2024-12-20 09:56:26 -0300

    html with js

--- section-2/comentarios/comentario.js
+++ section-2/comentarios/comentario.js
@@ -1,3 +1,9 @@
 console.log('Meu nome é "Marlon". Estou aprendendo JavaScript às', 9, 'da manhã')
 // isso é um comentario no codigo
 console.log('Ola mundo!') //Mais um comentario
+
+/* 
+Linha
+De
+Comentario
+*/
---
+++ section-2/html-with-js/js/index.js
@@ -0,0 +1,5 @@
+//Um Comentario
+console.log("Ola Mundo");
+console.log("Esse trecho será exibido no colsole do navegador");
+
+alert('Ola mundo')
