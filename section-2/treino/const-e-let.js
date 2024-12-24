cedc460ea2e6083ea3bc9a34b2aebe5969b1cdbe
marloikhodi <deivid.serafim03@gmail.com>
2024-12-24 09:55:55 -0300

    praticando let e const

---
+++ section-2/treino/const-e-let.js
@@ -0,0 +1,18 @@
+const nome = "Deivid"
+const sobrenome = "Marlon"
+const idade = 20
+const peso = 90
+const alturaEmM = 1.75
+let imc
+let anoNascimento
+
+imc = peso / (alturaEmM * alturaEmM)
+anoNascimento = 2024 - idade
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos e pesa ${peso} kg`)
+console.log(`Tem ${alturaEmM} seu IMC é de ${imc}`)
+console.log(`${nome} nasceu em ${anoNascimento}`)
+
+
+
+
