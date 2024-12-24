f1f1425b5b8ccc41f8bc4f009b2735bc917ac0eb
marloikhodi <deivid.serafim03@gmail.com>
2024-12-24 10:13:35 -0300

    valores primitivos

---
+++ section-2/tiposdedados/tiposdedados.js
@@ -0,0 +1,17 @@
+const nome = 'Marlo' // string
+const nome2 = "Marlo" // string
+const nome3 = `Marlo` // string
+const num1 = 10 // number
+const num2 = 10.52 // number
+let nomeAluno // undefined -> nao aponta pra local nenhum na memoria
+const sobrenomeAluno = null // nulo -> nao aponta pra local nenhum na memoria
+const aprovado = true // boolean = true or false 
+
+console.log(typeof nome, nome)
+console.log(typeof nome2, nome2)
+console.log(typeof nome3, nome3)
+console.log(typeof num1, num1)
+console.log(typeof num2, num2)
+console.log(typeof nomeAluno, nomeAluno)
+console.log(typeof sobrenomeAluno, sobrenomeAluno)
+console.log(typeof aprovado, aprovado)
