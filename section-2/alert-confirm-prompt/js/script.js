2e4a4bbf6c2120dd0204ac7348a3e6c5335ab5c0
marloikhodi <deivid.serafim03@gmail.com>
2025-01-01 20:10:23 -0300

    calculadora simples de JS

--- section-2/alert-confirm-prompt/js/script.js
+++ section-2/alert-confirm-prompt/js/script.js
@@ -1,3 +1,15 @@
-window.alert('Mensagem de teste')
-window.confirm('deseja cancelar?')
-window.prompt('Me diga seu nome')
+// window.alert('Mensagem de teste')
+// window.confirm('deseja cancelar?')
+// window.prompt('Me diga seu nome')
+
+// alert('Mensagem de teste')
+// confirm('deseja cancelar?')
+// prompt('Me diga seu nome')
+
+let numberone = prompt('digite um número')
+let numbertwo = prompt('digite outro número')
+
+numberone = Number(numberone)
+numbertwo = Number(numbertwo)
+
+alert(`O resultado da sua conta foi: ${numberone + numbertwo}`)
