39b5d7c66dad98c6aad7eab59a44526a2f8575d6
marloikhodi <deivid.serafim03@gmail.com>
2025-01-19 20:39:06 -0300

    strings

--- section-2/tiposdedados/tiposdedados.js
+++ section-2/tiposdedados/tiposdedados.js
@@ -15,3 +15,34 @@ console.log(typeof num2, num2)
 console.log(typeof nomeAluno, nomeAluno)
 console.log(typeof sobrenomeAluno, sobrenomeAluno)
 console.log(typeof aprovado, aprovado)
+//               0123456789... toda letra tem um indice, valor entro da string começando do 0
+const strings = "O rato roeu a roupa do rei de roma."
+
+console.log(strings[5])
+console.log(strings[12])
+console.log(strings.charAt(5))
+console.log(strings.charAt(12))
+
+console.log(strings.concat(" em", " um lindo dia"))
+
+console.log(`${strings} em um lindo dia`)
+
+console.log(strings.indexOf('t', 6))
+
+console.log(strings.match(/[a-z]/g))
+console.log(strings.match(/[a-z]/))
+
+console.log(strings.search(/t/))
+
+console.log(strings.replace("Um", "Outra"))
+console.log(strings.replace(/t/g, "saas"))
+
+console.log(strings.length) //tamanho do indice, contando o 0
+
+console.log(strings.slice(0, 2))
+console.log(strings.slice(-6, -1))
+
+console.log(strings.split(' ', 2))
+
+console.log(strings.toUpperCase())
+console.log(strings.toLowerCase())
