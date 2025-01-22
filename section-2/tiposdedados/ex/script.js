1ed5ae9c1c1ece146f98cbbb551a4e8c9fba404b
marloikhodi <deivid.serafim03@gmail.com>
2025-01-22 11:35:31 -0300

    using string templates

---
+++ section-2/tiposdedados/ex/script.js
@@ -0,0 +1,11 @@
+const name = prompt("Digite seu nome completo:")
+document.body.innerHTML += `Então seu nome é ${name}? </br>`
+
+document.body.innerHTML += `Seu nome tem ${name.length} letras <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: ${name.charAt(1)} <br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra I no seu nome? ${name.indexOf('i')}<br />`;
+document.body.innerHTML += `Qual o último índice da letra I no seu nome? ${name.lastIndexOf('i')}<br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${name.slice(-3)}<br />`;
+document.body.innerHTML += `As palavras do seu nome são: ${name.split(' ')}<br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: ${name.toUpperCase()}<br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: ${name.toLowerCase()}<br />`;
