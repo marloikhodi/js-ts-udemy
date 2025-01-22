2ef5d5da3501c942d0af9c1d3192c6576bea3313
marloikhodi <deivid.serafim03@gmail.com>
2025-01-22 14:46:51 -0300

    Objeto Math

---
+++ section-2/number/math/index.js
@@ -0,0 +1,12 @@
+let num1 = 9.12323
+let num2 = 2.123
+
+console.log(Math.sqrt(num1)) //square root
+console.log(Math.floor(num1)) //arredonda para baixo
+console.log(Math.ceil(num2)) //arredonda para cima
+console.log(Math.round(num1)) //arredonda para o mais proximo
+
+console.log(Math.max(20,2,3,40,20,3000,202))
+console.log(Math.min(20,2,3,40,20,3000,202))
+
+console.log (Math.random())
