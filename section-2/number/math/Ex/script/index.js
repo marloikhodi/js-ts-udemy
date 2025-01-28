1fba9626f4bdfc71b90d90a4a60b142c23bb356d
marloikhodi <deivid.serafim03@gmail.com>
2025-01-28 19:52:23 -0300

    comments

--- section-2/number/math/Ex/script/index.js
+++ section-2/number/math/Ex/script/index.js
@@ -10,5 +10,11 @@ texto.innerHTML = `
 <p>é NaN? ${Number.isNaN(numero)}</p>
 <p>Arredondado para baixo: ${Math.ceil(numero)}</p>
 <p>Arredondado para cima: ${Math.floor(numero)}</p>
-<p>Com duas casas deciamais: ${numero.toFixed(2)}</p>
+<p>Com duas casas deciamais: ${numero.toFixed(2)}</p> 
 `
+// .sqrt calcula a raiz quadrada
+// .isInteger verifica se é inteiro (sem decimais)
+// .isNan verifica se é ou nao um NaN retornando boolean
+// .ceil arredonda para cima
+// .floor arredonda para baixo
+// .toFixed limita casas decimais
