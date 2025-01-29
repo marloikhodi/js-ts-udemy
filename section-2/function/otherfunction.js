cd32db57f26f3cc91f440abbba9487a7b0d10668
marloikhodi <deivid.serafim03@gmail.com>
2025-01-29 20:53:08 -0300

    outros metodos de criar function

---
+++ section-2/function/otherfunction.js
@@ -0,0 +1,16 @@
+const raiz = function (n) {
+    return n ** 0.5
+}
+
+console.log(raiz(9))
+// metodo de criar função dentro de variavel
+
+const raiz2 = (n) => {
+    return n ** 0.5
+}
+
+// const raiz2 = n => n ** 0.5
+// com o nivel de simplicidade, pode ser reduzido somente a esse codigo
+console.log(raiz2(25))
+// metodo arrow function
+
--- section-2/function/soma.js
+++ section-2/function/soma.js
@@ -1,6 +1,6 @@
 function soma(x, y) {
     const resultado = x + y //oque está dentro da function nao interage com oque esta fora da function
-    return resultado
+    return resultado //return será o ultimo valor dentro da função, nada abaixo ira ser executado
 }
 
 console.log(soma(1, 2))
@@ -9,3 +9,7 @@ console.log(soma(10, 20))
 
 const resultado = soma(2, 2) // outra const resultado, caso em cima estivesse fora da function, daria erro
 console.log(resultado)
+
+const notNumber = soma() //esta sendo feita a soma de dois valores undefined
+console.log(notNumber)
+
