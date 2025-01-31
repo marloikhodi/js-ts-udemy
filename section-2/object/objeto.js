a1c8f1cc9cd554cb582d09be0459bf5977b5225f
marloikhodi <deivid.serafim03@gmail.com>
2025-01-30 22:40:14 -0300

    objeto com function

--- section-2/object/objeto.js
+++ section-2/object/objeto.js
@@ -1,16 +1,21 @@
 const pessoa1 = { //objeto literal
     nome: 'marlo',
     sobrenome: 'serafim',
-    idade: 20
+    idade: 20,
+
+    fala() {
+        console.log(`Ola, me chamo ${this.nome} ${this.sobrenome}, eu tenho ${this.idade} anos de idade`)
+    }
 }
 
-console.log(pessoa1.nome)
+pessoa1.fala()
 
 function criaPessoa(nome, sobrenome, idade) {
     return {
-        nome: nome,
-        sobrenome: sobrenome,
-        idade: idade
+        nome,// nome: nome,
+        sobrenome,// sobrenome: sobrenome,
+        idade// idade: idade
+        //quando o valor é igual ao parametro, nao tem a necessidade de repetição
     }
 }
 
