e83bbaae621dff2e563e1a1dd27f881b3c54b276
marloikhodi <deivid.serafim03@gmail.com>
2025-01-31 21:54:39 -0300

    exercicio com array + object

---
+++ section-2/arrays/ex/js/script.js
@@ -0,0 +1,35 @@
+function meuEscopo() {
+    const form = document.querySelector('.form')
+    const resultado = document.querySelector('.resultado')
+
+    const pessoas = []
+
+    // form.onsubmit = function (evento) {
+    //     evento.preventDefault();
+    //     alert('foi enviado');
+    //     console.log("foi enviado")
+    // }
+    // let contador = 1
+    function recebeEventoForm(evento) {
+        evento.preventDefault()
+        // console.log(`Form não enviado, tentativa número ${contador}`)
+        // contador++
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        })
+
+        console.log(pessoas)
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} <br> ${peso.value} ${altura.value}</p>`
+    }
+    form.addEventListener('submit', recebeEventoForm)
+}
+meuEscopo()
