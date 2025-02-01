744e46e2b70d9764d75ceca8f48cc19439264e6d
marloikhodi <deivid.serafim03@gmail.com>
2025-01-31 21:09:37 -0300

    exemplos

--- section-2/tiposdedados/maisdados.js
+++ section-2/tiposdedados/maisdados.js
@@ -1,17 +1,18 @@
 /*
-Primitivos (imutaveis) - string, number, boolean, undefined, null (biing, symbol) - valor
+Primitivos (imutaveis) - string, number, boolean, undefined, null (biing, symbol) - 
 
 Referencia (mutavel) - array, object, function 
 */
 
 let a = 'Uma coisa'
-let b = a
+let b = a //copia
 console.log(a, b) //Uma coisa Uma coisa
 a = 'Outra coisa'
 console.log(a, b) //Outra coisa Uma coisa
 
 let first = [1, 2, 3]
 let second = first
-console.log(first, second) //[ 1, 2, 3 ] [ 1, 2, 3 ]
+let third = [...first] //copia
+console.log(first, second, third) //[ 1, 2, 3 ] [ 1, 2, 3 ] [ 1, 2, 3 ]
 first.pop()
-console.log(first, second) //[ 1, 2 ] [ 1, 2 ]
+console.log(first, second, third) //[ 1, 2 ] [ 1, 2 ] [ 1, 2, 3 ]
