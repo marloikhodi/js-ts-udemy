5bc8b8eb8894bf726145e95ff39eea596d27040b
marloikhodi <deivid.serafim03@gmail.com>
2025-02-04 21:31:37 -0300

    operadores logicos

---
+++ section-2/operadores/index.js
@@ -0,0 +1,13 @@
+/*
+Operadoes de comparação
+> Maior que
+>= Maior que ou igual a
+< Menor que
+<= Menor que ou igual a 
+== Igualdade (somente checa valor) **nao recomendado
+===igualdade estrita (checa valor e tipo)
+!=diferente (somente checa valor) **nao recomendado
+!==diferente estrito (checa valor e tipo)
+*/ 
+
+console.log(10 > 5) //retorna um boolean, checando um dado *true
