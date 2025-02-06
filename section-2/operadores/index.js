ffa54628692a21482efeff147228493a534754c0
marloikhodi <deivid.serafim03@gmail.com>
2025-02-05 21:04:23 -0300

    Operadores

--- section-2/operadores/index.js
+++ section-2/operadores/index.js
@@ -4,10 +4,17 @@ Operadoes de comparação
 >= Maior que ou igual a
 < Menor que
 <= Menor que ou igual a 
-== Igualdade (somente checa valor) **nao recomendado
+== Igualdade (somente checa valor) **nao recomendado - faz uma correção de tipo
 ===igualdade estrita (checa valor e tipo)
-!=diferente (somente checa valor) **nao recomendado
+!=diferente (somente checa valor) **nao recomendado - faz uma correção de tipo
 !==diferente estrito (checa valor e tipo)
 */ 
 
 console.log(10 > 5) //retorna um boolean, checando um dado *true
+const menorque = 10 < 11
+console.log(menorque) //*true
+
+const valor1 = 10
+const valor2 = "10"
+console.log(valor1 == valor2) //*true
+console.log(valor1 === valor2) //*false - foi feito a verificação de tipo dos valores
