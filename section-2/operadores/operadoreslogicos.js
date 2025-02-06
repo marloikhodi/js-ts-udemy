f4603f2b62dc62d81d4e3c2f1e5d88a9a4711d6d
marloikhodi <deivid.serafim03@gmail.com>
2025-02-05 21:36:57 -0300

    and or not

--- section-2/operadores/operadoreslogicos.js
+++ section-2/operadores/operadoreslogicos.js
@@ -16,3 +16,17 @@ const possuodinheiro1 = false
 console.log(possuodinheiro1 || tenhocredito) //retorna *true pois uma das const sao verdadeiras, sendo feita a leitura, possuo dinheiro ou tenho credito
 
 console.log(!false) //retorna *true
+
+/*
+&& = false && true = false (assim que localiza uma expressão "false" ele retorna o valor literal que estiver "false", caso não tenha, retorna o ultimo valor true)
+valores "false" no JS = *False, 0, NaN, '',"",``, null, indefined (todos com exceção do false, são FALSY, valores que retornam falso quando necessario qualquer outro valor é true )
+|| = true || true = (retorna o primeiro valor "true" literal que localiza)
+*/
+
+console.log('nome' && 0 && 'nome2') // retorna *0 por ser avaliado fem false
+console.log('nome' && "nome3" && 'nome2') // retorna *nome2 por ser o ultimo valor true
+
+console.log(0 || NaN || "Marlo" || true)// retorna *Marlo por ser o primeiro true que localiza
+const corUsuario = null
+const corPadrao = "Red"
+console.log(corUsuario || corPadrao) //retorna *Red pois o "usuário" não setou uma cor ainda
