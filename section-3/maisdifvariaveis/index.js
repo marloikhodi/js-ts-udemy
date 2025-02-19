f4444732e696c80565ff59dafddef0508a1bb836
marloikhodi <deivid.serafim03@gmail.com>
2025-02-19 16:24:13 -0300

    mais diferença de var e let

--- section-3/maisdifvariaveis/index.js
+++ section-3/maisdifvariaveis/index.js
@@ -6,19 +6,32 @@ num = 3
 //var tem escopo de função 
 
 const verdadeira = true
-let nome = 'marlo' //criando 
-var nome2 = 'deivid' //criando 
+let nome = 'marlo' // criando 
+var nome2 = 'deivid' // criando 
 
 if (verdadeira) {
-    let nome = 'santos' //criando - caso no bloco da frente nao tivesse uma let nome, seria exibido esse
-    var nome2 = 'pedro' //redeclarando a var nome2
+    let nome = 'santos' // criando - caso no bloco da frente nao tivesse uma let nome, seria exibido esse
+    var nome2 = 'pedro' // redeclarando a var nome2
     // console.log(nome, nome2)
 
     if (verdadeira) {
-        let nome = 'serafim' //criando 
-        var nome2 = 'joao' //redeclarando a var nome2
-        //console.log(nome, nome2) // a let "nome" vai ser porcurado primeiramente no bloco atual, nao encontrando, vai movendo um bloco para trás
+        let nome = 'serafim' // criando 
+        var nome2 = 'joao' // redeclarando a var nome2
+        // console.log(nome, nome2) // a let "nome" vai ser porcurado primeiramente no bloco atual, nao encontrando, vai movendo um bloco para trás
     }
 }
 
 console.log(nome2)
+
+var falaBomDia = 'bom dia!'
+function falaOi() {
+    // var oi = 'oi'
+    // console.log(oi)
+    console.log(falaBomDia) // o var falaBomDia é exibido pois a function reconhece a varque está fora do seu escopo
+}
+
+// console.log(oi) // nao exibe o var, pois o "var oi" está protejido pela função
+falaOi()
+
+console.log(varDepois) //*undefined, a engine do JS eleva os VAR e FUNCTION sendo carregado antes do script, e quando solicitado adiciona valor, por esse motivo o return de undefined
+var varDepois = 'xisde'
