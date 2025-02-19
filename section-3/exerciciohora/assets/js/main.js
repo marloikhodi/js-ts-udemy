28eaa2782f69004803f7bc13b1e2c830485192c6
marloikhodi <deivid.serafim03@gmail.com>
2025-02-18 21:48:59 -0300

    terceira resolução

--- section-3/exerciciohora/assets/js/main.js
+++ section-3/exerciciohora/assets/js/main.js
@@ -1,100 +1,36 @@
 const h1 = document.querySelector(".container h1");
 const data = new Date();
 
-// function getDiaSemanaTexto(diaSemana) {
-//     let diaSemanaTexto;
-//     switch (diaSemana) {
-//         case 0:
-//             diaSemanaTexto = "domingo";
-//             return diaSemanaTexto; // or break
-//         case 1:
-//             diaSemanaTexto = "segunda-feira";
-//             return diaSemanaTexto; // or break
-//         case 2:
-//             diaSemanaTexto = "terça-feira";
-//             return diaSemanaTexto; // or break
-//         case 3:
-//             diaSemanaTexto = "quarta-feira";
-//             return diaSemanaTexto; // or break
-//         case 4:
-//             diaSemanaTexto = "quinta-feira";
-//             return diaSemanaTexto; // or break
-//         case 5:
-//             diaSemanaTexto = "sexta-feira";
-//             return diaSemanaTexto; // or break
-//         case 6:
-//             diaSemanaTexto = "sabado";
-//             return diaSemanaTexto; // or break
-//         default:
-//             diaSemanaTexto = "";
-//     }
-// }
+function getDiaSemanaTexto(diaSemana) {
+    const diasSemana = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado']
+    return diasSemana[diaSemana]
+}
 
-// function getNomeMes(numMes) {
-//     let diaMesTexto;
-//     switch (numMes) {
-//         case 0:
-//             diaMesTexto = "janeiro";
-//             return diaMesTexto; // or break
-//         case 1:
-//             diaMesTexto = "fevereiro";
-//             return diaMesTexto; // or break
-//         case 2:
-//             diaMesTexto = "março";
-//             return diaMesTexto; // or break
-//         case 3:
-//             diaMesTexto = "abril";
-//             return diaMesTexto; // or break
-//         case 4:
-//             diaMesTexto = "maio";
-//             return diaMesTexto; // or break
-//         case 5:
-//             diaMesTexto = "junho";
-//             return diaMesTexto; // or break
-//         case 6:
-//             diaMesTexto = "julho";
-//             return diaMesTexto; // or break
-//         case 7:
-//             diaMesTexto = "agosto";
-//             return diaMesTexto; // or break
-//         case 8:
-//             diaMesTexto = "setembro";
-//             return diaMesTexto; // or break
-//         case 9:
-//             diaMesTexto = "outubro";
-//             return diaMesTexto; // or break
-//         case 10:
-//             diaMesTexto = "novembro";
-//             return diaMesTexto; // or break
-//         case 11:
-//             diaMesTexto = "dezembro";
-//             return diaMesTexto; // or break
-//         default:
-//             diaMesTexto = "";
-//     }
+function getNomeMes(numMes) {
+    const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+    return meses[numMes]
+}
 
-// }
+function adcionaZero(num) {
+    return num >= 10 ? num : `0${num}`;
+}
 
-// function adcionaZero(num) {
-//     return num >= 10 ? num : `0${num}`;
-// }
+function criaData(data) {
+    const diaSemana = data.getDay();
+    const numMes = data.getMonth();
 
-// function criaData(data) {
-//     const diaSemana = data.getDay();
-//     const numMes = data.getMonth();
+    const nomeDia = getDiaSemanaTexto(diaSemana);
+    const nomeMes = getNomeMes(numMes);
 
-//     const nomeDia = getDiaSemanaTexto(diaSemana);
-//     const nomeMes = getNomeMes(numMes);
+    return (
+        `${nomeDia}, ${data.getDate()} de ${nomeMes} de ${data.getFullYear()} ${adcionaZero(data.getHours())}:${adcionaZero(data.getMinutes())}`
+    );
+}
 
-//     return (
-//         `${nomeDia}, ${data.getDate()} de ${nomeMes} de ${data.getFullYear()} ${adcionaZero(data.getHours())}:${adcionaZero(data.getMinutes())}`
-//     );
-// }
+h1.innerHTML = criaData(data);
 
-// h1.innerHTML = criaData(data);
-
-const opcoes = {
-    dateStyle: "full",
-    timeStyle: "short",
-};
-h1.innerHTML = data.toLocaleDateString("pt-BR", { dateStyle: "full" });
+// const opcoes = {
+//     dateStyle: "full",
+//     timeStyle: "short",
+// };
+// h1.innerHTML = data.toLocaleDateString("pt-BR", { dateStyle: "full" });
