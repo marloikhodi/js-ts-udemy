32098fee7320d3831c0f8e50eb19f32690a010fa
marloikhodi <deivid.serafim03@gmail.com>
2025-02-23 22:54:01 -0300

    a

--- section-3/atribuiçaodesestruraçao/index.js
+++ section-3/atribuiçaodesestruraçao/index.js
@@ -1,4 +1,4 @@
 const numeros = [1, 2, 3]
-const [a, b, c] = numeros //cada indice do array esta sendo atribuidos a uma variavel
+const [a, b, c] = numeros //cada indice do array esta sendo atribuidos a uma variavel, sendo assim, feito a atribuiçao por destruturação
 console.log(a, b, c)
 
