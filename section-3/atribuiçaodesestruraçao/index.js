664bbe09c60bedcafd07c7514a92946106ad18d0
marloikhodi <deivid.serafim03@gmail.com>
2025-02-24 23:25:12 -0300

    aula 49

--- section-3/atribuiçaodesestruraçao/index.js
+++ section-3/atribuiçaodesestruraçao/index.js
@@ -3,5 +3,7 @@
 // console.log(a, b, c)
 
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-const primeiroNumero = numeros[0]
-console.log(primeiroNumero)
+// const primeiroNumero = numeros[0]
+
+const [primeiroNumero, segundoNumero, terceiroNumero] = numeros
+console.log(primeiroNumero, segundoNumero, terceiroNumero)
