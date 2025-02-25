53ea9862c3553bc3d123f6d936732179ea00d2c5
marloikhodi <deivid.serafim03@gmail.com>
2025-02-24 23:40:29 -0300

    aula 49

--- section-3/atribuiçaodesestruraçao/index.js
+++ section-3/atribuiçaodesestruraçao/index.js
@@ -5,6 +5,17 @@
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 // const primeiroNumero = numeros[0]
 
-const [primeiroNumero, segundoNumero, terceiroNumero, ...resto] = numeros //cada const está pegando um indice do array, o "...resto" vai pegar tudo que falta no array
-console.log(primeiroNumero, segundoNumero, terceiroNumero)
-console.log(resto)
+const [primeiroNumero, segundoNumero, terceiroNumero, ...resto] = numeros //cada const está pegando um indice do array, o "...resto" vai pegar tudo que falta no array (rest operator {tambem chamado de spread operator})
+// console.log(primeiroNumero, segundoNumero, terceiroNumero)
+// console.log(resto)
+
+const [esseSim1, , esseSim2, , esseSim3, ...restante] = ['sim', 'nao', 'sim', 'nao', 'sim', 'nao']
+// console.log(esseSim1, esseSim2, esseSim3, restante)
+
+const numerosIndices = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+console.log(numerosIndices[1][2]) //retorna 6
