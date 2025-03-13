d2aa6b0f212a2ceae3564a21ad162e6adac93b96
marloikhodi <deivid.serafim03@gmail.com>
2025-03-12 23:45:32 -0300

    Aula 54

--- section-3/exerciciofor/assets/js/main.js
+++ section-3/exerciciofor/assets/js/main.js
@@ -6,14 +6,14 @@ const elementos = [
 ]
 
 const container = document.querySelector(".container")
-const div = document.createElement('div')
+const div = document.createElement('div') //criado uma div dentro da const div
 
 for (let i = 0; i < elementos.length; i++) {
     let { tag, texto } = elementos[i]
-    let tagCriada = document.createElement(tag)
-    let textoCriado = document.createTextNode(texto)
+    let tagCriada = document.createElement(tag) //insere a tag do elementos em uma let
+    let textoCriado = document.createTextNode(texto) //insere o texto do elementos em uma let
     tagCriada.appendChild(textoCriado) //appendChild, transforma o item em () como filho do pre . com isso, é criado o HTML
-    div.appendChild(tagCriada)
+    div.appendChild(tagCriada) //junta tudo criado na linha de cima, dentro da const div
 }
 
 container.appendChild(div)
