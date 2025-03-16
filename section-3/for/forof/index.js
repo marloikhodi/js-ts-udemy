5def1fd81c8f62096f197bddd9e12e0314eddb95
marloikhodi <deivid.serafim03@gmail.com>
2025-03-15 22:21:52 -0300

    aula 56

--- section-3/for/forof/index.js
+++ section-3/for/forof/index.js
@@ -1,5 +1,10 @@
 const nome = `Deivid Marlon`
+const nomes = [`Marlon`, `Santos`, `Serafim`]
 //for of funciona com variaveis iteraveis (possuem indices) 
 for (valor of nome) {
     console.log(valor)
+
+    valor === 'D' ? console.log(`Valor é D`) : null // o dado em valor, é exatamente o mesmo que está na const, ao invés de um indicie
 }
+
+nomes.forEach((valor, indicie, array) => { console.log(valor, indicie, array) }) //utilizado juntamente com arrays
