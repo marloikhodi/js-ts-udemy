25e6a8a280a9a233d204e97724a511d90b1301fd
marloikhodi <deivid.serafim03@gmail.com>
2025-03-16 22:16:02 -0300

    aula 58

--- section-3/while/while&dowhile.js
+++ section-3/while/while&dowhile.js
@@ -1,6 +1,27 @@
-let num = 0
-
-while (num <= 10) {
-    console.log(num)
-    num++
+function random(min, max) {
+    const r = Math.random() * (max - min) + min
+    return Math.floor(r)
 }
+
+let rand = random(0, 50)
+
+while (rand !== 10) { //o while primeiro verifica a condiçao, apos isso, executa o codigo
+    rand = random(0, 50)
+    console.log(rand)
+}
+
+console.log("###")
+
+do {
+    rand = random(0, 50)
+    console.log(rand) //o dowhile primeiro executa o codigo, apos isso, realiza a veriifcaçao da condiçao
+} while (rand !== 10);
+
+
+
+// let num = 0
+
+// while (num <= 10) {
+//     console.log(num)
+//     num++
+// }
