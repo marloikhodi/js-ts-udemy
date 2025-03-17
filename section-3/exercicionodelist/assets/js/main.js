63db7e920f5b1c210c3d5772b8c4dfe5be96204d
marloikhodi <deivid.serafim03@gmail.com>
2025-03-16 21:55:35 -0300

    aula 57

---
+++ section-3/exercicionodelist/assets/js/main.js
@@ -0,0 +1,12 @@
+const paragrafos = document.querySelector('.paragrafos')
+const ps = document.querySelectorAll('p')
+
+const estilosBody = getComputedStyle(document.body)
+const backgroundColorBody = estilosBody.backgroundColor
+
+for (let p of ps) {
+    p.style.backgroundColor = backgroundColorBody
+    p.style.color = '#fff'
+    p.style.padding = '8px'
+    p.style.borderRadius = '8px'
+}
