b6d5327cba2e4b8bba875b542818dc4eb96ab2e0
marloikhodi <deivid.serafim03@gmail.com>
2025-03-17 23:18:02 -0300

    aula 59

--- section-3/break&continue/index.js
+++ section-3/break&continue/index.js
@@ -1,10 +1,15 @@
 const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for (let n of num) {
-    if (n === 7) {
-        // break  //vai parar o sistema, chegando no 7, ela para por completo
-        continue //executa o codigo abaixo, pulando o 7
+    if (n === 2) {
+        console.log("sem número 2")
+        continue //executa o codigo abaixo, pulando o 7 (deve ser colocado antes da açao)
     }
 
     console.log(n)
+
+    if (n === 7) {
+        break //vai parar o sistema, chegando no 7, ela para por completo
+    }
+
 }
