de7910dbcd4830f3974773ebcaf7771bd9c4fbb1
marloikhodi <deivid.serafim03@gmail.com>
2025-03-18 22:23:26 -0300

    aula 60

---
+++ section-3/exercicios/ex1/index.js
@@ -0,0 +1,17 @@
+/// minha solução
+
+const num1 = 111
+const num2 = 1
+
+if (num1 > num2) {
+    console.log(num1)
+} else { console.log(num2) }
+
+///
+
+function high(x, y) {
+    x === y ? console.log("Os numeeros são iguais") : null
+    x > y ? console.log(x) : console.log(y)
+}
+
+high(20, 20)
