a4adb4945accdda53ce8031d7ebc1f22dd7f1100
marloikhodi <deivid.serafim03@gmail.com>
2025-03-20 21:28:23 -0300

    aula 61

---
+++ section-3/exercicios/ex2/index.js
@@ -0,0 +1,5 @@
+
+
+const ePaisagem = (altura, largura) => largura > altura //caso a funçao verifique true ou false, nao tem a necessidade de expecificar "? true : false"
+
+console.log(ePaisagem(200, 100))
