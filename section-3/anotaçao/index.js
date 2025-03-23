a5733eeed973daecce0cb8ef7d2a4b5eb8095768
marloikhodi <deivid.serafim03@gmail.com>
2025-03-22 21:42:11 -0300

    anotaçao

--- section-3/anotaçao/index.js
+++ section-3/anotaçao/index.js
@@ -1,2 +1,3 @@
-/* feito pesquisas em relação a POO, tive entendimento que JS é uma linguagem baseada em prototipos, diferente por exemplo, de java que é baseada em classes
+/* feito pesquisas em relação a POO, tive entendimento que JS é uma linguagem baseada em prototipos.
+diferente por exemplo, de java que é baseada em classes
 isso faz com que JS tenha muito mais felxibilidade */
