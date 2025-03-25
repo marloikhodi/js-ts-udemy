5b2b5ab9d28095ccf73153954e3c77d23b53cb4d
marloikhodi <deivid.serafim03@gmail.com>
2025-03-24 23:33:38 -0300

    aula 63

--- section-3/erroemjs/index.js
+++ section-3/erroemjs/index.js
@@ -1,9 +1,21 @@
-try {
-    console.log(naoExisto) //tentará realizar uma ação que pode acontecer um erro, caso nao consiga, realiza o catch
-} catch (err) {
-    console.log("naoExisto não existe")
-    console.log(err)
+// try {
+//     console.log(naoExisto) //tentará realizar uma ação que pode acontecer um erro, caso nao consiga, realiza o catch
+// } catch (err) {
+//     console.log("naoExisto não existe")
+//     console.log(err)
+// }
+
+function soma(x, y) {
+    if (typeof x !== "number" || typeof y !== "number") {
+        throw new Error(`x ou y precisam ser numeros`)
+    }
+
+    return x + y
 }
 
-
-
+try {
+    console.log(soma(1, 2))
+    console.log(soma('x', 2))
+} catch (error) {
+    console.log(error)
+}
