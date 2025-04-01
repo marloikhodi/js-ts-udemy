5c4694c5a4505d3ff7a47dd5fd3e26b2a07a8f0b
marloikhodi <deivid.serafim03@gmail.com>
2025-03-31 22:30:36 -0300

    aula 68

--- section-4/functions/index.js
+++ section-4/functions/index.js
@@ -1,5 +1,6 @@
 //declarar função (function hoisting)
 falaOi()
-function falaOi() { //funções declaradas dessa maneira sao prioridades no carregamento do motor JS, permitindo elas serem chamadas antes de serem feitas
+function falaOi() {
+    //funções declaradas dessa maneira sao prioridades no carregamento do motor JS, permitindo elas serem chamadas antes de serem feitas
     console.log('Oie')
 }
