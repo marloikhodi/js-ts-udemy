56f05d99d5d1981b3ebda68ab7510a8a285d8cd3
marloikhodi <deivid.serafim03@gmail.com>
2025-07-23 19:47:22 -0300

    xd

--- section-4/retorno/index.js
+++ section-4/retorno/index.js
@@ -1,11 +1,24 @@
 function newPerson(name, lastname) {
-    return { name, lastname }
+  return { name, lastname }
 }
 const firstPerson = newPerson('Marlon', 'Santos')
 console.log(firstPerson, typeof firstPerson)
 
 const secondPerson = {
-    name: 'Marlon',
-    lastname: 'Santos'
+  name: 'Marlon',
+  lastname: 'Santos'
 }
 console.log(secondPerson, typeof secondPerson)
+
+
+function criaMultiplicador(mutl) {
+  return function (n) {
+    return n * mutl
+  }
+}
+
+const duplica = criaMultiplicador(2)
+const triplica = criaMultiplicador(3)
+
+console.log(duplica(2))
+console.log(triplica(4))
