e2ff9a2a7719abe21cfcd059f96e6b622e2cd27a
marloikhodi <deivid.serafim03@gmail.com>
2025-09-02 00:04:56 -0300

    manipulando prototype

--- section-6/manipulandoPrototypes.js
+++ section-6/manipulandoPrototypes.js
@@ -16,5 +16,52 @@ objC.chaveC = "C";
 Object.setPrototypeOf(objB, objA);
 Object.setPrototypeOf(objC, objB);
 
-console.log(objB.chaveA);
-console.log(objC.chaveB);
+// console.log(objB.chaveA);
+// console.log(objC.chaveB);
+
+function Produto(nome, preco) {
+  this.nome = nome;
+  this.preco = preco;
+}
+
+Produto.prototype.desconto = function (porcentual) {
+  this.preco = this.preco - this.preco * (porcentual / 100);
+};
+
+Produto.prototype.aumento = function (porcentual) {
+  this.preco = this.preco + this.preco * (porcentual / 100);
+};
+
+const p1 = new Produto("Camisa", 50);
+p1.aumento(100);
+// console.log("preço aumentado em 100%:", p1.preco);
+
+const p2 = {
+  nome: "Caneca",
+  preco: 100,
+};
+
+Object.setPrototypeOf(p2, p1); //necessario informar a função e nao sua construtora
+
+p2.aumento(100);
+// console.log("preço aumentado em 100%:", p2.preco);
+
+const p3 = Object.create(Produto.prototype, {
+  preco: {
+    //informanado os valores por aqui, possui a necessidade de serem obj
+    writable: true,
+    enumerable: true,
+    configurable: true,
+    value: 110,
+  },
+  tamanho: {
+    writable: true,
+    enumerable: true,
+    configurable: true,
+    value: "M",
+  },
+});
+
+p3.nome = "Calça"; //posso informar os valores dessa maneira
+p3.aumento(10);
+console.log(p3);
