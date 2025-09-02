72514eb2f1e55059295daa18f60de3411cd2fbbf
marloikhodi <deivid.serafim03@gmail.com>
2025-09-01 23:19:25 -0300

    manipulando prototype

---
+++ section-6/manipulandoPrototypes.js
@@ -0,0 +1,20 @@
+//um obj literal é a mesma coisa de ter um new Object (funçao construtora) que possui um Object.prototype
+
+const objA = {
+  chaveA: "A",
+  //__proto__ === Object.prototype
+};
+
+const objB = {
+  chaveB: "B",
+  //__proto__ === objA
+};
+
+const objC = new Object();
+objC.chaveC = "C";
+
+Object.setPrototypeOf(objB, objA);
+Object.setPrototypeOf(objC, objB);
+
+console.log(objB.chaveA);
+console.log(objC.chaveB);
