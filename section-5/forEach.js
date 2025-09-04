67c65ce40b4c5556e5fab13145ba0402cd897613
marloikhodi <deivid.serafim03@gmail.com>
2025-09-03 22:34:56 -0300

    validador de cpf

--- section-5/forEach.js
+++ section-5/forEach.js
@@ -1,10 +1,10 @@
-const ar = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+const ar = [10, 20, 30, 40, 50, 60, 70, 80, 90];
 
-let num = 0
+let num = 0;
 ar.forEach((valor, indice, array) => {
-    num += valor
-    console.log(valor)
-    console.log(array[indice])
+  num += valor;
+  console.log(valor);
+  console.log(array[indice]);
 });
 
-console.log(num)
+console.log(num);
--- section-5/metodoSplice.js
+++ section-5/metodoSplice.js
@@ -1,9 +1,9 @@
-const numeros = [10, 20, 30, 40, 50, 60, 70]
+const numeros = [10, 20, 30, 40, 50, 60, 70];
 
 //nomes.splice(indice inicial atual, delete, elem1, elem2,elem3)
-numeros.splice(1, null, 80, 90, 100)
-console.log(numeros)
+numeros.splice(1, null, 80, 90, 100);
+console.log(numeros);
 
-const numero = numeros.splice(1, 1)
-console.log(numero)
-console.log(numeros)
+const numero = numeros.splice(1, 1);
+console.log(numero);
+console.log(numeros);
