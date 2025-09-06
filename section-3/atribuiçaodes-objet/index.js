0a474c258a21be6e3cb1675af6e6ae2e88e6ae62
marloikhodi <deivid.serafim03@gmail.com>
2025-09-05 23:23:06 -0300

    test

--- section-3/atribuiçaodes-objet/index.js
+++ section-3/atribuiçaodes-objet/index.js
@@ -1,22 +1,25 @@
 const pessoa = {
-    nome: `Deivid`,
-    sobrenome: `Marlon`,
-    idade: 20,
-    endereco: {
-        rua: `Av Brasil`,
-        numero: 28
-    }
-}
+  nome: `Deivid`,
+  sobrenome: `Marlon`,
+  idade: 20,
+  endereco: {
+    rua: `Av Brasil`,
+    numero: 28,
+  },
+};
 // atribuiçao normal
-//const nome = pessoa.nome 
+//const nome = pessoa.nome
 
 //Atribuição via desestruturação:
-const { nome = `placeholder`, sobrenome, idade: age } = pessoa
-console.log(`Me chamo ${nome} ${sobrenome} e tenho ${age} anos de idade!`)
+const { nome = `placeholder`, sobrenome, idade: age } = pessoa;
+console.log(`Me chamo ${nome} ${sobrenome} e tenho ${age} anos de idade!`);
 
-const { endereco: { rua, numero }, endereco } = pessoa
-console.log(`Moro na ${rua}, número ${numero}!`)
-console.log(endereco, `\n`)
+const {
+  endereco: { rua, numero },
+  endereco,
+} = pessoa;
+console.log(`Moro na ${rua}, número ${numero}!`);
+console.log(endereco, `\n`);
 
-const { idade, ...resto } = pessoa
-console.log(idade, resto) //o resto pega todas as const que nao foram utilizadas
+const { idade, ...resto } = pessoa;
+console.log(idade, resto); //o resto pega todas as const que nao foram utilizadas
