396b1abe20c04862cb2e876ae66dcd1db539aae4
marloikhodi <deivid.serafim03@gmail.com>
2025-09-09 23:59:57 -0300

    metodos em promise

---
+++ section-8/metodosPromise.js
@@ -0,0 +1,28 @@
+function rand(min, max) {
+  max *= 1000;
+  min *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function aguarde(msg, tempo) {
+  return new Promise((resolve, reject) => {
+    //seta o Promise com o construtor da promise e sempre vai ter dois parametros (comumente chamados de resolve e reject)
+    if (typeof msg !== "string") reject("Valor errado!");
+    setTimeout(() => {
+      resolve(msg);
+    }, tempo);
+  });
+}
+
+const promises = [
+  "Primeiro Valor",
+  aguarde("Promise 1", 3000),
+  aguarde("Promise 1", 500),
+  aguarde("Promise 1", 1000),
+  "Outro Valor",
+];
+
+Promise.all(promises).then((valor) => {
+  // .all vai realizar todas as Promises antes de retornar com o valor
+  console.log(valor);
+});
