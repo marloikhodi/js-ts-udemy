750312e8ed085c252492e3aaed186277462cb59f
marloikhodi <deivid.serafim03@gmail.com>
2025-09-16 20:05:17 -0300

    fixação

--- anotaçao/fixacao.js
+++ anotaçao/fixacao.js
@@ -1,15 +1,23 @@
-class Cachorro {
+class Animal {
   constructor(nome, raca) {
     this.nome = nome;
     this.raca = raca;
   }
+}
 
-  latir(vezes) {
-    for (let v = 0; v < vezes; v++) {
+class Cachorro extends Animal {
+  constructor(nome, raca, latido, cor) {
+    super(nome, raca);
+    this.latido = latido;
+    this.cor = cor;
+  }
+
+  latir() {
+    for (let v = 0; v < this.latido; v++) {
       console.log(`O cachorro ${this.nome} está latindo!`);
     }
   }
 }
 
-const bob = new Cachorro("Bob", "Vira lata");
+const bob = new Cachorro("Bob", "Vira lata", 3, "caramelo");
 bob.latir(3);
