1b00d080ad06134a7f3be0a0def5a934697b7719
marloikhodi <deivid.serafim03@gmail.com>
2025-09-17 18:13:34 -0300

    babel

--- anotaçao/index.js
+++ anotaçao/index.js
@@ -1,3 +1,5 @@
 /* feito pesquisas em relação a POO, tive entendimento que JS é uma linguagem baseada em prototipos.
 diferente por exemplo, de java que é baseada em classes
 isso faz com que JS tenha muito mais felxibilidade */
+
+/* Babel é um pacote que auxilia no desenvolvimento, atualizando o codigo para versões mais antigas e compativeis do JS para navegadores antigos */
